5a5d4c5d256e422655befdbc91c571002829b482
Preshy-Jones <adedibuprecious@gmail.com>
2021-07-04 12:35:50 +0100

    configured browser-router with gihub pages

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ library.add(fab, fas,far, faCheckSquare, faCoffee)
 function App() {
   return (
   
-    <Router>
+    <Router basename={process.env.PUBLIC_URL} >
     <div className="App">
       <NavBar/>
           <Switch>
