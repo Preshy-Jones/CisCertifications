74f8addbe0affb319d27f48330016fffd8fef8d1
Preshy-Jones <adedibuprecious@gmail.com>
2021-07-04 14:18:37 +0100

    test

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -8,10 +8,10 @@ function navBar() {
           <img src={logo} alt="" />
       </div>
       <ul className='navbar-list'>
-        <li className='nav-item'><a href="/">Home</a></li>
-        <li className='nav-item'><a href="/#/about">About Us</a></li>
-        <li className='nav-item'><a href="/#/accreditation">Accreditation</a></li>
-        <li className='nav-item'><a href="/#/contact">Contact</a></li>
+        <li className='nav-item'><a href="/CisCertifications/#">Home</a></li>
+        <li className='nav-item'><a href="/CisCertifications/#/about">About Us</a></li>
+        <li className='nav-item'><a href="/CisCertifications/#/accreditation">Accreditation</a></li>
+        <li className='nav-item'><a href="/CisCertifications/#/contact">Contact</a></li>
       </ul>
     </div>
   )
