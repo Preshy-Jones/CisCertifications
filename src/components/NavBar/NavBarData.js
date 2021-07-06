60c12c00920db2990f4d42061533c9e9b329bf16
Preshy-Jones <adedibuprecious@gmail.com>
2021-07-06 11:07:49 +0100

    navbar dissappear when clicked

---
+++ src/components/NavBar/NavBarData.js
@@ -0,0 +1,24 @@
+const NavBarData = [
+  {
+   name:'Home',
+   link:'/CisCertifications/#/' 
+  },
+  {
+    name:'About Us',
+    link:'/CisCertifications/#/about' 
+   },
+   {
+    name:'Services',
+    link:'/CisCertifications/#/services' 
+   },
+   {
+    name:'Accreditation',
+    link:'/CisCertifications/#/accreditation' 
+   },
+   {
+    name:'Contact',
+    link:'/CisCertifications/#/contact' 
+   },
+]
+
+export default NavBarData
--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import logo from '../images/sislogo.png';
+import logo from '../../images/sislogo.png';
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import NavBarData from './NavBarData'
+
 
 function NavBar() {
 
@@ -21,11 +23,15 @@ function NavBar() {
           {state.clicked ?<FontAwesomeIcon className='times' icon={['fas', 'times']} /> : <FontAwesomeIcon className='bars' icon={['fas', 'bars']} /> }
       </div>
       <ul className= {state.clicked? 'navbar-list active':'navbar-list'} >
-        <li className='nav-item'><a href="/CisCertifications/#">Home</a></li>
-        <li className='nav-item'><a href="/CisCertifications/#/about">About Us</a></li>
-        <li className='nav-item'><a href="/CisCertifications/#/services">Services</a></li>
-        <li className='nav-item'><a href="/CisCertifications/#/accreditation">Accreditation</a></li>
-        <li className='nav-item'><a href="/CisCertifications/#/contact">Contact</a></li>
+        {NavBarData.map(data=>{
+          return(
+          <li className='nav-item'>
+            <a href={data.link} onClick={handleClick} >
+            {data.name}
+            </a>
+          </li>
+          )
+        })}
       </ul>
     </div>
   )
